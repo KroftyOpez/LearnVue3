d81e565e07e3dd8aba11f50bb7ad110e0da21758
Krofty <dima112831@gmail.com>
2024-04-05 18:10:48 +0700

    Познакомились с директивами

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+<footer class="bg-dark">
+  <p class="p-4">Copyright © Евсейка Димка Витальевич, 2024</p>
+</footer>
+</template>
+
+<style scoped>
+p{
+  color: whitesmoke;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav class="navbar navbar-dark bg-dark">
+      <ul class="d-flex">
+        <li class="nav-item"><RouterLink class="nav-link" to="/">Главная</RouterLink></li>
+        <li class="nav-item"><RouterLink class="nav-link" to="/directives">Знакомство с директивами</RouterLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+ ul {
+   list-style-type: none;
+ }
+  a {
+    color: whitesmoke;
+  }
+  a:hover {
+    color: #5599ee;
+  }
+</style>
