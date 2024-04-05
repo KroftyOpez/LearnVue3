f1419b7b190fc3e2e1228ef85b8bc1ee457628f2
Krofty <dima112831@gmail.com>
2024-04-05 19:15:06 +0700

    Познакомились с работой API и динамическими маршрутоами

---
+++ src/views/ApiDetailsView.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div class = "d-flex flex-wrap p-4">
+    <div class="card m-2">
+      <div class="card-header">{{product.title}}</div>
+      <div class="card-body">
+        <img :src="product.thumbnail" :alt="product.title"/>
+        <p>Описание товара: {{ product.description }}</p>
+      </div>
+      <div class="card-footer d-flex justify-content-between"></div>
+      <div class="fw-bold">Цена: ${{product.price}}</div>
+      <div><RouterLink class="btn btn-dark" to="/api">Назад</RouterLink></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .card {
+    width: 500px;
+  }
+  .card img{
+    width: 450px;
+    margin: 0 auto;
+  }
+</style>
+
+<script>
+export default {
+  //Переменные
+  data(){
+    return {
+      product:null,
+    }
+  },
+  //Метод выполняется после добавления компонента в DOM
+  created() {
+     const id = this.$route.params.id
+    const url = "https://dummyjson.com/products/" + id;
+     fetch(url)
+         .then(response => response.json())
+         .then(data => this.product = data)
+
+  }
+}
+</script>
---
+++ src/views/ApiView.vue
@@ -0,0 +1,55 @@
+
+
+<template>
+ <section class="m-4 bg-gradient bg-success">
+   <h2 class="text-center">Товары для чайников</h2>
+   <div class = "d-flex flex-wrap p-4 justify-content-center">
+     <div class="card m-2" v-for="product in products">
+       <div class="card-header">{{product.title}}</div>
+       <div class="card-body">
+         <img :src="product.thumbnail" :alt="product.title"/>
+       </div>
+       <div class="card-footer d-flex justify-content-between"></div>
+       <div class="fw-bold">Цена: ${{product.price}}</div>
+       <div><RouterLink class="btn btn-dark" :to="'/api/' + product.id">Подробнее</RouterLink></div>
+     </div>
+   </div>
+
+
+
+ </section>
+</template>
+
+<style scoped>
+.card {
+  width: 300px;
+}
+.card img{
+  width: 280px;
+  margin: 0 auto;
+}
+</style>
+
+<script>
+export default {
+  //Переменные
+  data() {
+    return{
+      products: [],
+    }
+  },
+  //Методы
+  methods:{
+    getProducts(){
+      fetch('https://dummyjson.com/products')
+          .then(response => response.json())
+          .then(data => this.products = data.products)
+    }
+  },
+  //Метод выполняется при загрузке страницы
+  created() {
+    this.getProducts()
+  }
+}
+
+</script>
